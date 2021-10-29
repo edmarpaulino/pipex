1c7e77788d9102ab02132e651af4a6bae5879bc9
Edmar Paulino <edmarpaulino9@gmail.com>
2021-10-29 11:40:31 -0300

    change error message in close_pipe function

--- sources/shared/ft_close_pipe.c
+++ sources/shared/ft_close_pipe.c
@@ -6,7 +6,7 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 10:37:09 by edpaulin          #+#    #+#             */
-/*   Updated: 2021/10/28 10:45:51 by edpaulin         ###   ########.fr       */
+/*   Updated: 2021/10/29 11:39:56 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int	ft_close_pipe(int *end)
 
 	ret_value = 0;
 	if (close(end[0]) == FT_ERROR)
-		ret_value = ft_print_error_message(NULL);
+		ret_value = ft_print_error_message("end[0]");
 	if (close(end[1]) == FT_ERROR)
-		ret_value = ft_print_error_message(NULL);
+		ret_value = ft_print_error_message("end[1]");
 	return (ret_value);
 }
